1c66c873942becef8a63d288b4ad32dfbb9fd43b
seanyoung247 <seanyoung247@gmail.com>
2022-07-16 19:21:46 +0100

    feat: Adds initial pathing and movement code

--- assets/js/modules/objects.js
+++ assets/js/modules/objects.js
@@ -15,7 +15,13 @@ export class MapObject {
 
     /** Registers a collision with another object */
     collide(obj) {
-
+        /* 
+           This will be called when a moving object moves on to the same 
+           tile as this object. The object colliding will be passed as obj.
+           So this is where any collision actions can be taken. For instance
+           if a derived class needs to increase the player's health, we'd
+           check the obj is the player then increase the player health here.
+        */
     }
 }
 
@@ -26,6 +32,31 @@ export class MapMover extends MapObject {
     constructor(x, y, map, speed) {
         super(x, y, map);
         this._speed = speed;
+        this._path = [];
+        this._goal = null;
+        this._vector = {x:0, y:0};
+    }
+
+    createPath(goal) {
+        if (goal != this._goal) {
+            this._path = this._map.getPath(this._tile, goal);
+        }
+    }
+
+    update(time) {
+        /*
+          This method will be called every frame so the object can update itself.
+         */
+    }
+
+    setVector(x, y) {
+        this._vector.x = Math.sign(x);
+        this._vector.y = Math.sign(y);
+    }
+
+    move(time) {
+        // Move along the vector based on speed and time passed.
+        
     }
 }
 
@@ -36,4 +67,6 @@ export class Player extends MapMover {
     constructor(x, y, map, speed) {
         super(x, y, map, speed);
     }
+
+
 }
