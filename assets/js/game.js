25b32fee419a17f5288fe44be7e7974c60595619
seanyoung247 <seanyoung247@gmail.com>
2022-07-16 04:46:52 +0100

    feat: Adds game-map element

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,20 +23,24 @@
         // a whole chunk of HTML and add it in one big operation rather than lots of little
         // ones. This is much faster and more efficient.
         const frag = new DocumentFragment();
+        const mapel = document.createElement('div');
+        mapel.classList.add('game-map');
+        frag.append(mapel);
         
         // Now create a div for each tile on the map and add it to the document fragment
         for (let i = 0; i < totalTiles; i++) {
             // We'll probably want to set up tiles as walls or passable here by reading the map
             const tile = document.createElement('div');
             tile.classList.add('game-tile');
-            frag.append(tile);
+            mapel.append(tile);
         }
         // Create the element that will represent the player
         const player = document.createElement('div');
         player.classList.add('game-player');
         frag.append(player);
         // More for loops for enemies, map items etc here.
-        // Add the document fragment to the page
+
+        // Finally add the document fragment to the page
         document.getElementById('game-screen').append(frag);
     }
 
