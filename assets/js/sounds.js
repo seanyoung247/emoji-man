1eb5f2a0b4fd759d25380333608241e18a003267
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-07-16 14:06:21 +0000

    Changing format of audio

--- assets/js/sounds.js
+++ assets/js/sounds.js
@@ -2,7 +2,7 @@
 export var soundfx = {
     gameSong: new Howl({
         src: [
-            'assets/sounds/retrorace-108750.mp3',
+            'assets/sounds/retrorace-108750.ogg',
             'assets/sounds/retrorace-108750.wav'
         ],
         autoplay: true,
@@ -11,21 +11,21 @@ export var soundfx = {
     }),
     gameStart: new Howl({
         src: [
-            'assets/sounds/emojiman_beginning.wav',
+            'assets/sounds/emojiman_beginning.ogg',
             'assets/sounds/emojiman_beginning.mp3'
         ]
 
     }),
     chomp: new Howl({
         src: [
-            'assets/sounds/emojiman_chomp.wav',
+            'assets/sounds/emojiman_chomp.ogg',
             'assets/sounds/emojiman_chomp.mp3'
         ]
 
     }),
     eatfruit: new Howl({
         src: [
-            'assets/sounds/emojiman_eatfruit.mp3',
+            'assets/sounds/emojiman_eatfruit.ogg',
             'assets/sounds/emojiman_eatfruit.wav'
         ]
     })
