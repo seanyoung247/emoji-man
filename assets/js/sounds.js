4d9a6a6705374257a8965ef6937be51ec9335271
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-07-16 14:22:36 +0000

    Changing format of audio correction

--- assets/js/sounds.js
+++ assets/js/sounds.js
@@ -7,26 +7,45 @@ export var soundfx = {
         ],
         autoplay: true,
         loop: true,
-        volume: 0.5
+        volume: 0.1
     }),
     gameStart: new Howl({
         src: [
             'assets/sounds/emojiman_beginning.ogg',
-            'assets/sounds/emojiman_beginning.mp3'
-        ]
+            'assets/sounds/emojiman_beginning.wav'
+        ],
 
     }),
     chomp: new Howl({
         src: [
             'assets/sounds/emojiman_chomp.ogg',
-            'assets/sounds/emojiman_chomp.mp3'
-        ]
+            'assets/sounds/emojiman_chomp.wav'
+        ],
+        volume: 0.1
 
     }),
     eatfruit: new Howl({
         src: [
             'assets/sounds/emojiman_eatfruit.ogg',
             'assets/sounds/emojiman_eatfruit.wav'
+        ]
+    }),
+    eat: new Howl({
+        src: [
+            'assets/sounds/emojiman_eat.ogg',
+            'assets/sounds/emojiman_eat.wav'
+        ]
+    }),
+    death: new Howl({
+        src: [
+            'assets/sounds/emojiman_death.ogg',
+            'assets/sounds/emojiman_death.wav'
+        ]
+    }),
+    eatGhost: new Howl({
+        src: [
+            'assets/sounds/emojiman_eatghost.ogg',
+            'assets/sounds/emojiman_eatghost.wav'
         ]
     })
 }
